a1e6f7c334a684dbab27ac4da4d93523960caa9a
Wadson Pereira <wadson.ayres@gmail.com>
2024-03-10 22:05:44 -0300

    Ajusta balance column para previnir valores negativos na base de dados

--- database/migrations/2024_03_09_163620_create_account_table.php
+++ database/migrations/2024_03_09_163620_create_account_table.php
@@ -11,7 +11,7 @@ return new class extends Migration {
         Schema::create('account', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->decimal('balance', 10, 2);
+            $table->decimal('balance', 10, 2)->unsigned();
             $table->timestamps();
         });
     }
